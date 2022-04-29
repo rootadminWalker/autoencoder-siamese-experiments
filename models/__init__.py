17681e15328885b834289b0e580a7b7bf4a72f04
rootadminwalker <chiioleong519@gmail.com>
2022-04-29 17:27:07 +0800

    Models to train market1501 dataset

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,6 @@
-from .SimpleAutoEncoder import SimpleAutoEncoder
-from .ConvolutionAutoEncoder import ConvolutionAutoEncoder
+# from .SimpleAutoEncoder import SimpleAutoEncoder
+# from .ConvolutionAutoEncoder import ConvolutionAutoEncoder
+from .Market1501TripletModel import Market1501TripletModel, Market1501TripletModelEval, Market1501TripletMiniVGG, \
+    Market1501TripletMiniVGGEval
 from .SimpleConvSiamese import ConvSiamese, OldConvSiamese
 from .SimpleConvTriplet import ConvTriplet
