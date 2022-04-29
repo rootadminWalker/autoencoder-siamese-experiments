8db11c87af7a11bad59b4e96447747d1735c459c
rootadminwalker <chiioleong519@gmail.com>
2022-04-30 00:34:21 +0800

    Add Market1501TripletMiniVGG Relu before BatchNormalization version

--- models/Market1501TripletModel.py
+++ models/Market1501TripletModel.py
@@ -238,6 +238,121 @@ class Market1501TripletMiniVGGEval(Market1501TripletMiniVGG):
         return self.model(image)
 
 
+class Market1501TripletMiniVGGReluBeforeBN(nn.Module):
+    INITIAL_FILTERS = 32
+    MODEL_NAME = "Mini-VGG"
+
+    def __init__(
+            self,
+            input_shape,
+            embedding_dim,
+            conv_blocks,
+            conv_kernel_size=(3, 3),
+            max_pool_kernel_size=(2, 2),
+            dropout_rate=0.03,
+            filters=64
+    ):
+        super(Market1501TripletMiniVGGReluBeforeBN, self).__init__()
+        self.model_name = "Mini-VGG"
+
+        start_block = self.__conv_block(
+            in_channels=input_shape[1],
+            out_channels=Market1501TripletMiniVGG.INITIAL_FILTERS,
+            conv_kernel_size=conv_kernel_size,
+        )
+        self.backbone = nn.Sequential(start_block)
+
+        assert conv_blocks % 2 == 0, "Conv blocks must be an even number in MiniVGGNet"
+        for idx in range(2, conv_blocks + 1):
+            current_filters_multiply = int(round((idx / 2) + 0.1, 0))
+            current_filters = Market1501TripletMiniVGG.INITIAL_FILTERS * current_filters_multiply
+
+            _, last_output_channels, _, _ = self.__get_last_shape(input_shape, self.backbone)
+            self.backbone.add_module(f'Block:{idx}', self.__conv_block(
+                in_channels=last_output_channels,
+                out_channels=current_filters,
+                conv_kernel_size=conv_kernel_size,
+            ))
+            if idx % 2 == 0:
+                self.backbone.add_module(f'Pool:{current_filters_multiply}', self.__pool_block(
+                    max_pool_kernel_size=max_pool_kernel_size,
+                    dropout_rate=dropout_rate
+                ))
+
+        self.backbone.add_module('Flatten', nn.Flatten())
+        backbone_output_features = self.__get_last_shape(input_shape, self.backbone)[-1]
+        self.fully_connected = nn.Sequential(
+            nn.Linear(in_features=backbone_output_features, out_features=512),
+            nn.BatchNorm1d(num_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=128),
+            nn.BatchNorm1d(num_features=128),
+            nn.ReLU(),
+            nn.Linear(in_features=128, out_features=embedding_dim)
+        )
+        self.model = nn.Sequential(self.backbone, self.fully_connected)
+
+    @staticmethod
+    def __get_last_shape(input_shape, block):
+        return block(torch.rand(input_shape)).shape
+
+    @staticmethod
+    def __pool_block(max_pool_kernel_size, dropout_rate):
+        return nn.Sequential(
+            nn.MaxPool2d(kernel_size=max_pool_kernel_size),
+            nn.Dropout(p=dropout_rate)
+        )
+
+    @staticmethod
+    def __conv_block(in_channels, out_channels, conv_kernel_size):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=conv_kernel_size,
+                padding='same',
+                bias=True
+            ),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, anchor, positive, negative):
+        anchor_emd = self.model(anchor)
+        positive_emd = self.model(positive)
+        negative_emd = self.model(negative)
+
+        anchor_positive_dist = F.pairwise_distance(anchor_emd, positive_emd)
+        anchor_negative_dist = F.pairwise_distance(anchor_emd, negative_emd)
+
+        return anchor_positive_dist, anchor_negative_dist
+
+
+class Market1501TripletMiniVGGReluBeforeBnEval(Market1501TripletMiniVGGReluBeforeBN):
+    def __init__(
+            self,
+            input_shape,
+            embedding_dim,
+            conv_blocks,
+            conv_kernel_size=(3, 3),
+            max_pool_kernel_size=(2, 2),
+            dropout_rate=0.03,
+            filters=64
+    ):
+        super(Market1501TripletMiniVGGReluBeforeBnEval, self).__init__(
+            input_shape=input_shape,
+            embedding_dim=embedding_dim,
+            conv_blocks=conv_blocks,
+            conv_kernel_size=conv_kernel_size,
+            max_pool_kernel_size=max_pool_kernel_size,
+            dropout_rate=dropout_rate,
+            filters=filters
+        )
+
+    def forward(self, image):
+        return self.model(image)
+
+
 if __name__ == '__main__':
     from torchinfo import summary
 
